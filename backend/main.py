b825ff9831761d967042a0921a31afcc1c5c2e7b
DiegoLinares11 <lin221256@uvg.edu.gt>
2025-12-10 08:02:44 -0600

    pass

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,7 @@ DB_CONFIG = {
     "host": os.getenv("DB_HOST", "localhost"),
     "database": os.getenv("DB_NAME", "boteo_db"),
     "user": os.getenv("DB_USER", "postgres"),
-    "password": os.getenv("DB_PASSWORD", ".linares11"),
+    "password": os.getenv("DB_PASSWORD", "postgres"),
     "port": os.getenv("DB_PORT", "5432")
 }
 
